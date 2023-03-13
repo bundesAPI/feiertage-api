109ac6056417484011daae9e49b211b74720a417
bundesbot <bundesAPI@users.noreply.github.com>
2023-03-13 09:59:29 +0000

    Generate newest code from openapi.yaml

--- python-client/sphinx-docs/conf.py
+++ python-client/sphinx-docs/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath("../"))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "feiertage-api"
-copyright = "2022, bund.dev"
+copyright = "2023, bund.dev"
 author = "bund.dev"
 
 version = "1.0.1"
